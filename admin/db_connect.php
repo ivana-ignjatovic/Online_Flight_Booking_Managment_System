a23ece02b284dde8558d1de54afdc84e898b23b5
Ivana Ignjatovic <ivkauzice00@gmail.com>
2023-06-14 21:01:56 +0200

    All files

---
+++ admin/db_connect.php
@@ -0,0 +1,3 @@
+<?php 
+
+$conn= new mysqli('booking-flight-db.cprtlu45dga1.us-east-1.rds.amazonaws.com','ivana97','ivana9797','flight_booking_db')or die("Could not connect to mysql".mysqli_error($con));
